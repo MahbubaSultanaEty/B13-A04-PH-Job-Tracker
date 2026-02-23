a0c20e3550ac36a333d1c1c4c7dae6ea14e94702
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-02-23 18:57:24 +0600

    stored all the needed id's in variables

---
+++ script.js
@@ -0,0 +1,38 @@
+let totalCount = document.getElementById("total-count");
+let interviewCount = document.getElementById("interview-count");
+let rejectedCount = document.getElementById("rejected-count");
+console.log(totalCount, interviewCount, rejectedCount);
+
+const allCard = document.getElementById('all-card');
+
+let interviewList = [];
+let rejectedList = [];
+
+function calculateCount() {
+    totalCount.innerText = allCard.children.length;
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
+}
+calculateCount();
+
+const allFilterBtn = document.getElementById("all-filter-btn");
+const interviewFilterBtn = document.getElementById("interview-filter-btn");
+const rejectedFilterBtn = document.getElementById("rejected-filter-btn");
+
+function toggleBtn(id) {
+     allFilterBtn.classList.remove("bg-blue-950", "text-white")
+    interviewFilterBtn.classList.remove("bg-blue-950", "text-white")
+    rejectedFilterBtn.classList.remove("bg-blue-950", "text-white");
+
+    allFilterBtn.classList.add("bg-white", "text-black")
+    interviewFilterBtn.classList.add("bg-white", "text-black") 
+    rejectedFilterBtn.classList.add("bg-white", "text-black");
+
+    let selected = document.getElementById(id);
+    selected.classList.add("bg-blue-950", "text-white");
+    selected.classList.remove("bg-white", "text-black");
+
+}
+
+const mainContainer = document.querySelector("main");
+
